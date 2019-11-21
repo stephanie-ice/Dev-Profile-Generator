be2139fccafa21676675585b4bb383da39c861e8
Stephanie Ice <stephanie.icee@gmail.com>
2019-11-21 10:12:57 -0800

    init and pseudocode

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log ("Yay, node is working!");
+
+var fs = require("fs");
+
+//find modules to do the heavy lifting for this app, and install them
+
+//key words github, profile, generator, scrape
+
+//module inquirer to prompt user ot input fav color, .then use that color for the PDF background
+
+//follow documentation to do the following:
+
+//scrape github for:
+    //username
+    //icon or image
+    //links listed on gitlab
+    //user bio
+    //number of repos
+    //number of followers
+    //number of github stars
+    //number of users following
+
+//writeFile with above info to a PDF file in this folder
+
+//??figure out how to make the PDF file styled and present the info the way you want.
